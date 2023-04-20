998b49ce2095e1b21a42ff7c9a6046138b65dee4
ɨֆǟǟƈ ɖɛ ʟɛօռ <deleonisaac978@gmail.com>
2023-04-19 20:43:03 -0500

    capstone

---
+++ cap/src/credentials.h
@@ -0,0 +1,5 @@
+//************************* Adafruit.io Setup *****************************************
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883        // use 1883 for SSL
+#define AIO_USERNAME    "Isaacdeleon223"	// replace with your Adafruit.io username
+#define AIO_KEY         "aio_GRfh73dXZ1ym2k2JV9iG7w9WqJs3"		    // replace with your Adafruit.io key
